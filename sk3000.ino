f9e2c35da6ebaf1907051da974f43e6d55118848
Steven Klise <steven@stevenklise.com>
2011-12-08 16:40:42 -0500

    16 toggle switches wired. Skip and Rest modes implemented.

--- sk3000.ino
+++ sk3000.ino
@@ -8,6 +8,9 @@ int elapsedTime = 0;
 int tempo = 200;
 int skipStates[] = {0,0,0,0,0,0,0,0};
 
+int melody[] = {
+  110, 131, 165, 196, 247, 294, 349, 440};
+
 int tempoUpState = 0;
 int tempoDownState = 0;
 int tempoPrevUpState = 0;
@@ -22,31 +25,48 @@ void setup() {
 }
 
 void loop() {
-  muxDigitalWrite(1,currentStep%8,HIGH);
-  tone(SOUNDOUT,map(muxAnalogRead(0,currentStep), 0, 700, 0, 1100));
   
-  elapsedTime = millis() - startTime;
-  if(elapsedTime >= tempo) {
-    for(int i=0;i<8;i++) {
-      skipStates[i] = muxDigitalRead(2,i);
+  // TOGGLE SWITCH DEBUG
+//  for(int i=0; i<16;i++){
+//    Serial.print(muxDigitalRead(2,i));
+//    Serial.print(',');
+//  }
+//  Serial.println();
+    muxDigitalWrite(1,currentStep%8,HIGH);
+    // tone(SOUNDOUT,map(muxAnalogRead(0,currentStep), 0, 700, 0, 1100));
+
+    if(muxDigitalRead(2,currentStep+8) == 1) {
+      noTone(SOUNDOUT);
+    } else {
+      tone(SOUNDOUT,melody[currentStep]);
     }
-    muxDigitalWrite(1,currentStep%8,LOW);
-    currentStep = nextStep(nextStepMode(), currentStep, maxSteps);
-    startTime = millis();
-  }
-
-  tempoDownState = muxDigitalRead(0,10);
-  tempoUpState = muxDigitalRead(0,11);
-  if(tempoDownState != tempoPrevDownState && tempoDownState == HIGH) {
-    Serial.println("TempoDown");
-    tempo += 10;
-  }
-  if(tempoUpState != tempoPrevUpState && tempoUpState == HIGH) {
-    Serial.println("TempoUp");
-    tempo -= 10;
-    if (tempo <= 10) tempo = 10;
-  }
-
-  tempoPrevDownState = tempoDownState;
-  tempoPrevUpState = tempoUpState;
+    Serial.println(melody[currentStep]);
+    
+    elapsedTime = millis() - startTime;
+    
+    if(elapsedTime >= tempo) {
+      for(int i=0;i<8;i++) {
+        skipStates[i] = muxDigitalRead(2,i);
+      }
+      muxDigitalWrite(1,currentStep%8,LOW);
+      currentStep = nextStep(nextStepMode(), currentStep, maxSteps);
+      startTime = millis();
+    }
+    
+    tempoDownState = muxDigitalRead(0,10);
+    tempoUpState = muxDigitalRead(0,11);
+    
+    if(tempoDownState != tempoPrevDownState && tempoDownState == HIGH) {
+      Serial.println("TempoDown");
+      tempo += 10;
+    }
+    
+    if(tempoUpState != tempoPrevUpState && tempoUpState == HIGH) {
+      Serial.println("TempoUp");
+      tempo -= 10;
+      if (tempo <= 10) tempo = 10;
+    }
+    
+    tempoPrevDownState = tempoDownState;
+    tempoPrevUpState = tempoUpState;
 }
